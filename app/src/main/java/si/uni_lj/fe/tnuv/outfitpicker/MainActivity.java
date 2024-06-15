56c62f57021e7c83cb81f92029e4e8ac9e8e5fc1
nat4lija <ivanovicnatalija03@gmail.com>
2024-06-15 14:30:27 +0200

    Commit

--- app/src/main/java/si/uni_lj/fe/tnuv/outfitpicker/MainActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/outfitpicker/MainActivity.java
@@ -1,30 +1,44 @@
 package si.uni_lj.fe.tnuv.outfitpicker;
 
 import android.os.Bundle;
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView navView = findViewById(R.id.bottom_navigation);
+        // Set up Navigation Controller
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
 
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+        // Set up AppBarConfiguration with top-level destinations
+        appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_color_picker, R.id.navigation_closet, R.id.navigation_profile)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+        // Set up ActionBar with Navigation Controller
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+
+        // Set up Bottom Navigation View with Navigation Controller
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        NavigationUI.setupWithNavController(bottomNav, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
--- app/src/main/java/si/uni_lj/fe/tnuv/outfitpicker/ui/ClosetFragment.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/outfitpicker/ui/ClosetFragment.java
@@ -1,7 +1,13 @@
 package si.uni_lj.fe.tnuv.outfitpicker.ui;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,9 +18,16 @@ public class ClosetFragment extends Fragment {
         // Required empty public constructor
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_closet, container, false);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Example of triggering navigation programmatically
+        navigateToColorPickerFragment();
+    }
+
+    private void navigateToColorPickerFragment() {
+        // Navigate to ClosetFragment
+        NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment);
+        navController.navigate(R.id.navigation_color_picker);
     }
 }
